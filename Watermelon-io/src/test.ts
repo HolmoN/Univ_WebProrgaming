2d87375920fae665c210c71ed520f8df862bdcac
HolmoN <noppo3062@gmail.com>
2023-12-11 12:23:23 +0900

    test.tsを元に戻した

--- Watermelon-io/src/test.ts
+++ Watermelon-io/src/test.ts
@@ -1,4 +1,16 @@
-window.onload = () => 
-{
+import * as Matter from "matter-js";
+import { Canvas, MatterEnvironment } from "./modules/MatterEnvironment";
 
+window.onload = () => {
+    console.log("gg");
+    const container = document.querySelector<HTMLElement>(".container");
+    if (container == null) return;
+
+    let canvas: Canvas = new Canvas(container, 420, 700);
+    MatterEnvironment.Init(canvas);
+
+    container.addEventListener("click", () => {
+        let bubble: Matter.Body = Matter.Bodies.circle(MatterEnvironment.width/2, 5, 10);
+        MatterEnvironment.Instantiate(bubble);
+    });
 }
