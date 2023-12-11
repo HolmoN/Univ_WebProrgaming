eaf5b772abe62df2681e7b211635b141111545a1
HolmoN <noppo3062@gmail.com>
2023-12-11 19:33:21 +0900

    Revert "testの削除"
    This reverts commit 827665d26fbf9640c7adcbebd71fe91699969e1d.

---
+++ Watermelon-io/src/test.ts
@@ -0,0 +1,21 @@
+import * as Matter from "matter-js";
+import { Canvas, MatterEnvironment } from "./modules/MatterEnvironment";
+import { BubbleRaw } from "./modules/BubbleRaw";
+
+window.onload = () => {
+    console.log("gg");
+    const container = document.querySelector<HTMLElement>(".container");
+    if (container == null) return;
+
+    let canvas: Canvas = new Canvas(container, 420, 700);
+    MatterEnvironment.Init(canvas);
+
+    container.addEventListener("click", () => {
+        //let bubble: Matter.Body = Matter.Bodies.circle(MatterEnvironment.width/2, 5, 10);
+        const bubble: BubbleRaw = new BubbleRaw(MatterEnvironment.width / 2, 5, 30);
+        //MatterEnvironment.Instantiate(bubble.Body);
+
+        const b2 = bubble;
+        MatterEnvironment.Instantiate(b2.Body);
+    });
+}
