14ddec6154df932ad62b96b8107f9410b5d5b4f5
cre0m0puff <arieshuuu777@gmail.com>
2023-12-15 02:27:52 +0900

    スタートボタンを押す→ボタン消える→Matching...表示

--- Watermelon-io/src/TitleScene.ts
+++ Watermelon-io/src/TitleScene.ts
@@ -2,6 +2,7 @@ import { ITitleScene } from "./Interface/ITitleScene"
 export class TitleScene implements ITitleScene {
 
     private _buttonElement :HTMLButtonElement;
+    private _matchingText :HTMLElement;
 
     /**
      * スタートボタンのオブジェクトを返す
@@ -12,12 +13,27 @@ export class TitleScene implements ITitleScene {
     
     constructor(){
         // HTML要素の取得
-        this._buttonElement = document.getElementById('myButton') as HTMLButtonElement;
+        this._buttonElement = document.getElementById('start') as HTMLButtonElement;
+        this._matchingText = document.getElementById('matchingText')as HTMLElement;
+        
+        
 
         // クリックイベントのリスナーを登録
         this._buttonElement.addEventListener('click', (event: MouseEvent) => {
             console.log("ボタンがクリックされました");
+            //ボタンがクリックされたらボタンが消える
+            this._buttonElement.className = "hidden";
+            
+            //マッチングを開始する
+
+            
+            this._matchingText.style.display = 'block';
+            
+            
+
         });
+
+        
 
     }
 
@@ -27,13 +43,30 @@ export class TitleScene implements ITitleScene {
      * 画面を描写するかを切り替える
      */
     Display(display: boolean): void {
+        if(display = true){
 
+        }
+        else{
+
+        }
     }
 
     /**
      * マッチングしたかどうかの結果を受け取る
      */
     Matched(matched: boolean): void {
+        if(matched = false){
+            this.Display(true);
+            /*
+            const repaint = async () => {
+                for (let i = 0; i < 2; i++) {
+                    await new Promise(resolve => requestAnimationFrame(resolve));
+                }
+            };
+            */
+        }
+        else{
 
+        }
     }
 }
