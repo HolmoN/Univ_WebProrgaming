17b60a2b253ca705351ae070000db430bd1d4a6e
AraiKantarou1 <araikantarou1883@gmail.com>
2023-12-16 01:17:53 +0900

    Gauge作成
    interfaceに新しくsetGaugeSizeを追加した。
    Gauge.tsを作成した。
    ・Displayをfalseにした際に謎のバーが出てしまう
    ・cssは仮です

--- Watermelon-io/src/test.ts
+++ Watermelon-io/src/test.ts
@@ -1,3 +1,18 @@
-window.onload = () => {
-    
-}
+import { TestGauge } from "./Gauge";
+
+// TestGauge クラスのインスタンスを作成
+const gauge = new TestGauge();
+
+gauge.setGaugeSize(20, 200);
+
+gauge.Display(true);
+
+// テスト用にゲージのランダムな変化を実現する関数
+const updateFillAmount = () => {
+    const newFillAmount = Math.random() * 3 - 1;
+    gauge.FillAmount = newFillAmount;
+    console.log('Updated FillAmount:', newFillAmount);
+};
+
+// （テスト用）500ミリ秒ごとにゲージのフィル量を変更
+setInterval(updateFillAmount, 500); 
