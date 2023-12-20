92b55a581c3d71f9dc4203916845808aa6849636
HolmoN <noppo3062@gmail.com>
2023-12-20 20:07:24 +0900

    うごかんなー

--- Watermelon-io/src/modules/Bubbles/Bubble_Lv1.ts
+++ Watermelon-io/src/modules/Bubbles/Bubble_Lv1.ts
@@ -3,7 +3,7 @@ import { BubbleRaw } from './BubbleRaw';
 export class Bubble_Lv1 extends BubbleRaw {
     //バブルの定義をする
     public constructor(x: number, y: number) {
-        super(x, y, 10);
+        super(x, y, 15);
         this.body.label = "bubble_1";
 
         //画像の貼り付けを行う
