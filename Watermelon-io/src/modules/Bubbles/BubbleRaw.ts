3490400c17a189abf8bda3ed5d86a1b7d4df2b36
HolmoN <noppo3062@gmail.com>
2023-12-20 22:07:59 +0900

    バブルを生成し落とせるようになった
    Next要検証
    それから絶対POS設置も要検証

--- Watermelon-io/src/modules/Bubbles/BubbleRaw.ts
+++ Watermelon-io/src/modules/Bubbles/BubbleRaw.ts
@@ -15,12 +15,28 @@ export class BubbleRaw {
     return this.body;
   }
 
+  public get x(): number {
+    return this.body.position.x;
+  }
+  public get y(): number {
+    return this.body.position.y;
+  }
+
+  ///----------
+  ///メソッド
+  ///----------
+
   //バブルの定義をする
   public constructor(x: number, y: number, r: number) {
     this.body = Matter.Bodies.circle(x, y, r, {
       label: "bubble_",
     });
     this.body.render.fillStyle = "blue";
+  }
+
+  //バブルの位置をセットする
+  public SetPosition(x: number, y:number){
+    Matter.Body.setPosition(this.body, {x, y});
   }
 
   //衝突処理はここでは行わない
