7a7f16b62967fdccdd65fe253bff53c8b32b6db6
HolmoN <noppo3062@gmail.com>
2023-12-21 09:53:29 +0900

    終了　ディレクトリの整理

--- Watermelon-io/src/test.ts
+++ Watermelon-io/src/test.ts
@@ -1,18 +1,1 @@
-import { Gauge } from "./Gauge";
 
-// TestGauge クラスのインスタンスを作成
-const gauge = new Gauge();
-
-gauge.setGaugeSize(20, 200);
-
-gauge.Display(false);
-
-// テスト用にゲージのランダムな変化を実現する関数
-const updateFillAmount = () => {
-    const newFillAmount = Math.random() * 3 - 1;
-    gauge.FillAmount = newFillAmount;
-    console.log('Updated FillAmount:', newFillAmount);
-};
-
-// （テスト用）500ミリ秒ごとにゲージのフィル量を変更
-setInterval(updateFillAmount, 500); 
