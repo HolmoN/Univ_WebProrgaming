77ec6485d0b7c78b2b9f40466d1a4683559f262b
HolmoN <noppo3062@gmail.com>
2023-12-27 10:20:05 +0900

    BubbleGeneratorの作成を行った

--- Watermelon-io/src/modules/Bubbles/Bubble_Lv3.ts
+++ Watermelon-io/src/modules/Bubbles/Bubble_Lv3.ts
@@ -3,7 +3,7 @@ import { BubbleRaw } from './BubbleRaw';
 export class Bubble_Lv3 extends BubbleRaw {
     //バブルの定義をする
     public constructor(x: number, y: number) {
-        super(x, y, 25);
+        super(x, y, 35);
         this.body.label = "bubble_3";
 
         //画像の貼り付けを行う
--- Watermelon-io/src/test.ts
+++ Watermelon-io/src/test.ts
@@ -6,34 +6,34 @@ import { Bubble_Lv1 } from "./modules/Bubbles/Bubble_Lv1";
 import { BubbleGenerator } from "./BubbleGenerator";
 
 window.onload = () => {
-    const container = document.querySelector<HTMLElement>(".container");
-    if (container == null) return;
+   const container = document.querySelector<HTMLElement>(".container");
+   if (container == null) return;
 
-    let canvas: Canvas = new Canvas(container, 420, 700);
-    MatterEnvironment.Init(canvas);
+   let canvas: Canvas = new Canvas(container, 420, 700);
+   MatterEnvironment.Init(canvas);
 
-    //壁の生成
-    const walls: Walls = new Walls();
-    walls.objects.forEach(element => {
-        MatterEnvironment.Instantiate(element);
-    });
+   //壁の生成
+   const walls: Walls = new Walls();
+   walls.objects.forEach(element => {
+      MatterEnvironment.Instantiate(element);
+   });
 
-    const bubbleGenerator = new BubbleGenerator();
+   const bubbleGenerator = new BubbleGenerator();
 
-    bubbleGenerator.SendController(new Bubble_Lv1(MatterEnvironment.width / 2, 5));
+   bubbleGenerator.SendController(new Bubble_Lv1(MatterEnvironment.width / 2, 5));
 
-    
-    //クsリックでボタンの生成
-    container.addEventListener("click", () => {
-        //let bubble: Matter.Body = Matter.Bodies.circle(MatterEnvironment.width/2, 5, 10);
-        //const bubble: BubbleRaw = new Bubble_Lv1(MatterEnvironment.width/2, 300);
-        //const bubble2: BubbleRaw = new Bubble_Lv1(MatterEnvironment.width/2-5, 30);
-        //MatterEnvironment.Instantiate(bubble.Body);
 
-        
+   //クリックでボタンの生成
+   container.addEventListener("click", () => {
+      //let bubble: Matter.Body = Matter.Bodies.circle(MatterEnvironment.width/2, 5, 10);
+      //const bubble: BubbleRaw = new Bubble_Lv1(MatterEnvironment.width/2, 300);
+      //const bubble2: BubbleRaw = new Bubble_Lv1(MatterEnvironment.width/2-5, 30);
+      //MatterEnvironment.Instantiate(bubble.Body);
 
-        //MatterEnvironment.Instantiate(bubble.Body);
-        //bubble.SetPosition(MatterEnvironment.width/2, 50);
-    });
-    
+      //ubbleGenerator.SetAbsolutePos(new Bubble_Lv1(MatterEnvironment.width / 4, 5), MatterEnvironment.width / 4, 5, false);
+
+      //MatterEnvironment.Instantiate(bubble.Body);
+      //bubble.SetPosition(MatterEnvironment.width/2, 50);
+   });
+
 }
