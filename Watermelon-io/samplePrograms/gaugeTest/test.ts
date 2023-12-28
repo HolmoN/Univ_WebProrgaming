8da1289f0da26630eefb044643655cc9dd1883a5
HolmoN <noppo3062@gmail.com>
2023-12-28 13:08:29 +0900

    gaugeTestの単体動作確認ができるようにした

---
+++ Watermelon-io/samplePrograms/gaugeTest/test.ts
@@ -0,0 +1,22 @@
+/*
+import { TestGauge } from "./Gauge";
+
+// TestGauge クラスのインスタンスを作成
+const gauge = new TestGauge();
+
+gauge.setGaugeSize(20, 200);
+
+gauge.Display(true);
+
+gauge.FillAmount = 0.5;
+
+// テスト用にゲージのランダムな変化を実現する関数
+const updateFillAmount = () => {
+    const newFillAmount = Math.random() * 3 - 1;
+    gauge.FillAmount = newFillAmount;
+    console.log('Updated FillAmount:', newFillAmount);
+};
+
+// （テスト用）500ミリ秒ごとにゲージのフィル量を変更
+setInterval(updateFillAmount, 500); 
+*/
