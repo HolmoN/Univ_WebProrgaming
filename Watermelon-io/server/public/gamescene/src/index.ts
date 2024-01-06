d6418df07f3df0b294d1e9846358452a1e46086e
HolmoN <noppo3062@gmail.com>
2024-01-06 14:47:23 +0900

    hoge

--- Watermelon-io/server/public/gamescene/src/index.ts
+++ Watermelon-io/server/public/gamescene/src/index.ts
@@ -5,7 +5,6 @@ import { Walls } from "./modules/Walls";
 import { Bubble_Lv1 } from "./modules/Bubbles/Bubble_Lv1";
 import { BubbleGenerator } from "./BubbleGenerator";
 import { WatermelonGame } from "./WatermelonGame";
-import { WatermelonGame_Debug } from "./WatermelonGame_Debug";
 import { Bubble_Lv2 } from "./modules/Bubbles/Bubble_Lv2";
 import { Bubble_Lv3 } from "./modules/Bubbles/Bubble_Lv3";
 import { Bubble_Lv4 } from "./modules/Bubbles/Bubble_Lv4";
@@ -22,17 +21,6 @@ import { io, Socket } from 'socket.io-client';
 import { Result } from "./Result";
 
 window.onload = () => {
-
-   /*
-   document.addEventListener('keydown', (event) => {
-      if (event.key === 'W' || event.key === 'w') {
-         const sqlTest :ISQLHandler = new ResultSave();
-         sqlTest.SetData("2024-12-12", "huga", "500");
-         console.log("wが押された");
-      }
-   });
-   */
-
    const container = document.querySelector<HTMLElement>(".container");
    if (container == null) return;
 
@@ -41,23 +29,11 @@ window.onload = () => {
 
    //ゲームの初期化
    const watermelonGame = new WatermelonGame();
-   //const watermelonGame = new WatermelonGame_Debug();
    watermelonGame.Play(() => { });
 
-   //クリック
-   container.addEventListener("click", () => {
-      //let bubble: Matter.Body = Matter.Bodies.circle(MatterEnvironment.width/2, 5, 10);
-      //const bubble: BubbleRaw = new Bubble_Lv1(MatterEnvironment.width/2, 300);
-      //const bubble2: BubbleRaw = new Bubble_Lv1(MatterEnvironment.width/2-5, 30);
-      //MatterEnvironment.Instantiate(bubble.Body);
-
-      //ubbleGenerator.SetAbsolutePos(new Bubble_Lv1(MatterEnvironment.width / 4, 5), MatterEnvironment.width / 4, 5, false);
-
-      //MatterEnvironment.Instantiate(bubble.Body);
-      //bubble.SetPosition(MatterEnvironment.width/2, 50);
-   });
-
    //キー入力
+   //デバッグ用
+   /*
    document.addEventListener('keydown', (event) => {
       if (event.code === 'Space') {
          console.log("スペースが押された");
@@ -66,53 +42,6 @@ window.onload = () => {
       if (event.code === 'Enter') {
          console.log("エンターが押された");
          watermelonGame.Stop();
-      }
-   /*
-      if(event.code === 'Space'){
-         const result = new Result()
-         result.Play(0, () => {
-            console.log("リザルト終了");
-         });
-      }
-      */
-   });
-
-   /*
-   document.addEventListener('keydown', (event) => {
-      //各種バブルの生成
-      if (event.key === '1') {
-         watermelonGame.BubbleGenerator.SendController(new Bubble_Lv1(MatterEnvironment.width / 2, 5), () => { });
-         console.log("1が押された");
-      }
-      if (event.key === '2') {
-         watermelonGame.BubbleGenerator.SendController(new Bubble_Lv2(MatterEnvironment.width / 2, 5), () => { });
-      }
-      if (event.key === '3') {
-         watermelonGame.BubbleGenerator.SendController(new Bubble_Lv3(MatterEnvironment.width / 2, 5), () => { });
-      }
-      if (event.key === '4') {
-         watermelonGame.BubbleGenerator.SendController(new Bubble_Lv4(MatterEnvironment.width / 2, 5), () => { });
-      }
-      if (event.key === '5') {
-         watermelonGame.BubbleGenerator.SendController(new Bubble_Lv5(MatterEnvironment.width / 2, 5), () => { });
-      }
-      if (event.key === '6') {
-         watermelonGame.BubbleGenerator.SendController(new Bubble_Lv6(MatterEnvironment.width / 2, 5), () => { });
-      }
-      if (event.key === '7') {
-         watermelonGame.BubbleGenerator.SendController(new Bubble_Lv7(MatterEnvironment.width / 2, 5), () => { });
-      }
-      if (event.key === '8') {
-         watermelonGame.BubbleGenerator.SendController(new Bubble_Lv8(MatterEnvironment.width / 2, 5), () => { });
-      }
-      if (event.key === '9') {
-         watermelonGame.BubbleGenerator.SendController(new Bubble_Lv9(MatterEnvironment.width / 2, 5), () => { });
-      }
-      if (event.key === 'Q' || event.key === 'q') {
-         watermelonGame.BubbleGenerator.SendController(new Bubble_Lv10(MatterEnvironment.width / 2, 5), () => { });
-      }
-      if (event.key === 'W' || event.key === 'w') {
-         watermelonGame.BubbleGenerator.SendController(new Bubble_Lv11(MatterEnvironment.width / 2, 5), () => { });
       }
    });
    */
